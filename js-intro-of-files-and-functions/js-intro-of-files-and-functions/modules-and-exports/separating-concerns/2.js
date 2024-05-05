ec06a14e34bedf494829faa9949fdc7341083743
raj14619 <raj14619@gmail.com>
2024-05-05 02:18:40 +0100

    commit
    commit

--- js-intro-of-files-and-functions/js-intro-of-files-and-functions/modules-and-exports/separating-concerns/2.js
+++ js-intro-of-files-and-functions/js-intro-of-files-and-functions/modules-and-exports/separating-concerns/2.js
@@ -1,4 +1,5 @@
 const { check, runTest } = require("../../test-api/index.js");
+const {addNumbers,multiplyNumbers,squareNumber,divideNumbers,isEven,getSquareRoot} = require("../separating-concerns/utils/mathFunctions.js")
 
 /*
 Complete the function below. It will take two numbers.
@@ -14,6 +15,12 @@ HINT: You will need to export multiple functions from the same file
 
 function isTotalEven(num1, num2) {
   // remember to use 2 util functions to complete this task
+
+  totalOfNum1AndNum2 = addNumbers(num1,num2);
+
+  return isEven(totalOfNum1AndNum2)
+
+
 }
 
 runTest("isTotalEven() should return appropriate boolean for total ", function () {
