0492c432e3257a25018d2f89e8d00c38d379ccc1
raj14619 <raj14619@gmail.com>
2024-05-05 09:24:29 +0100

    restructure

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Northcoders
  Northcoders
+ 
+ COMPLETED TASKS:
+ /home/raj/Desktop/Northcoders/Tasks/js-intro-of-files-and-functions (COMPLETED)
+ 
+ 
+ INCOMPLETE TASKS:
+ /home/raj/Desktop/Northcoders/Tasks/js-intro-problem-solving (INCOMPLETE) - CONTINUE FROM S3 Q3
--- Tasks/js-intro-problem-solving (INCOMPLETE)/section-3/3-find-missing-letter.js
+++ Tasks/js-intro-problem-solving (INCOMPLETE)/section-3/3-find-missing-letter.js
@@ -1,6 +1,22 @@
 const { check, runTest, skipTest } = require("../test-api/index.js");
 
 function findMissingLetter(letters) {
+
+  let sum = letters[0].toLowerCase().charCodeAt()-96 -  (letters[1].toLowerCase().charCodeAt()-96)
+
+
+
+  for(let i = 0; i < letters.length-1; i++){
+   if(letters[i].toLowerCase().charCodeAt()-96 - (letters[i+1].toLowerCase().charCodeAt()-96) === -1){
+      //console.log(letters[i].toLowerCase().charCodeAt()-96 - (letters[i+1].toLowerCase().charCodeAt()-96))
+    }else{
+    letters[i].toLowerCase().charCodeAt()-96 - (letters[i+1].toLowerCase().charCodeAt()-96 === -2)
+    return letters[i].toLowerCase().charCodeAt()-95
+    //console.log(letters[i].toLowerCase().charCodeAt()-96 - (letters[i+1].toLowerCase().charCodeAt()-96))
+    }
+  }
+  return "";
+
   /*
 In this function, you need to find out which letter is missing. But you can't use a reference lookup table (i.e. no array or object with the whole alphabet in it) so you will have think outside the box!
 
@@ -16,7 +32,7 @@ runTest("returns an empty string if no letters are missing", function () {
   check(findMissingLetter(["A", "B", "C", "D", "E"])).isEqualTo("");
 });
 
-skipTest("returns a missing capital letter", function () {
+runTest("returns a missing capital letter", function () {
   check(findMissingLetter(["A", "B", "C", "E"])).isEqualTo("D");
 });
 
